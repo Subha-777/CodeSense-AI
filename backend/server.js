34fa9e2848f06492381c356d8d247f4e448a23ac
Subha <arulsubha918@gmail.com>
2026-09-02 12:38:21 +0530

    Use dynamic PORT for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const User = require("./models/User");
 const adminRoutes = require("./routes/admin");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const upload = multer();
 
 app.use(cors());
